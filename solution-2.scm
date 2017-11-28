9b3c203ebbc0f30506a1b899ee3abc9707440c98
Rafał Pocztarski <r.pocztarski@gmail.com>
2017-11-28 16:33:16 +0100

    Refactor solution 2

--- solution-2.scm
+++ solution-2.scm
@@ -1,2 +1,3 @@
-(define f2 (lambda (a) (lambda (b) (lambda (c)
-  (((a (lambda (d) (lambda (e) (e (d b))))) (lambda (f) c)) (lambda (g) g))))))
+(define (f2 a) (lambda (b) (lambda (c)
+  (((a (lambda (d) (lambda (e) (e (d b)))))
+    (lambda (f) c)) (lambda (g) g)))))
