b1d94db447f2e1322f8c6809b6838e19aa3e6760
Rafał Pocztarski <r.pocztarski@gmail.com>
2017-11-28 16:33:08 +0100

    Refactor solution 1

--- solution-1.scm
+++ solution-1.scm
@@ -1,3 +1,3 @@
 (define (e a b c) (if (= c 0) a (e (b a) b (- c 1))))
 (define (f1 a) (lambda (b) (lambda (c)
-	(e c b (- ((a (lambda (a) (+ a 1))) 0) 1)))))
+  (e c b (- ((a (lambda (a) (+ a 1))) 0) 1)))))
