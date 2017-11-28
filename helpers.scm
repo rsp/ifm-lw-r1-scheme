897628b189885ed66c56ab13288c9fc035dcc847
Rafał Pocztarski <r.pocztarski@gmail.com>
2017-11-28 14:31:07 +0100

    Add sample input and count helper

---
+++ helpers.scm
@@ -0,0 +1,11 @@
+(define input1 (lambda (x) (lambda (y) (x y))))
+
+(define input2 (lambda (x) (lambda (y) (x (x y)))))
+
+(define input3 (lambda (x) (lambda (y) (x (x (x (x (x (x (x (x (x (x y))))))))))
+
+(define input4 (lambda (x) (input3 (input4 x))))
+
+(define input5 (lambda (x) (input3 (input4 x))))
+
+(define count (lambda (a) ((a (lambda (x) (+ x 1))) 0)))
