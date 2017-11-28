799c682fcfbd91e89001490bf67523e8fccf93e8
Rafał Pocztarski <r.pocztarski@gmail.com>
2017-11-28 16:19:58 +0100

    Update solution

--- solution-1.scm
+++ solution-1.scm
@@ -1,2 +1,3 @@
-(define e (lambda (a) (lambda (b) (lambda (c) (if (= c 0) a (((e (b a)) b) (- c 1)))))))
-(define f1 (lambda (a) (lambda (b) (lambda (c) (- (((e c) b) ((a (lambda (a) (- a 1))) 0)) 1)))))
+(define (e a b c) (if (= c 0) a (e (b a) b (- c 1))))
+(define (f1 a) (lambda (b) (lambda (c)
+	(e c b (- ((a (lambda (a) (+ a 1))) 0) 1)))))
